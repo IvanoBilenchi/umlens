64bd5ee802274653d4bd94864dd520c0e0ce7c48
Ivano Bilenchi <ivanobilenchi@gmail.com>
2019-04-08 17:13:07 +0200

    Remove pattern_type property

--- patternmatcher/matcher.py
+++ patternmatcher/matcher.py
@@ -1,7 +1,7 @@
 import re
 from abc import ABC, abstractmethod
 from itertools import chain
-from typing import Iterable, List, Type
+from typing import Iterable, List
 
 from .classdiagram import AggType, Class, Diagram, Multiplicity, RelRole
 from .pattern import (
@@ -12,30 +12,16 @@ from .pattern import (
 class Matcher(ABC):
     """Matcher abstract class."""
 
-    @property
-    @abstractmethod
-    def pattern_type(self) -> Type[Pattern]:
-        return Pattern
-
     @abstractmethod
     def match(self, dg: Diagram, cls: Class) -> Iterable[Pattern]:
         pass
-
-    @property
-    def pattern_name(self) -> str:
-        return self.pattern_type.__name__
 
 
 class MultiMatcher(Matcher):
     """Matches patterns using multiple base matchers."""
 
-    @property
-    def pattern_type(self) -> Type[Pattern]:
-        return Pattern
-
     def __init__(self, *args):
         self._matchers: List[Matcher] = list(args)
-        self._matchers.sort(key=lambda m: m.pattern_name)
 
     def match(self, dg: Diagram, cls: Class) -> Iterable[Pattern]:
         yield from (p for m in self._matchers for p in m.match(dg, cls))
@@ -43,10 +29,6 @@ class MultiMatcher(Matcher):
 
 class AbstractFactoryMatcher(Matcher):
     """Abstract factory matcher."""
-
-    @property
-    def pattern_type(self) -> Type[Pattern]:
-        return AbstractFactory
 
     def __init__(self, factory_method_matcher: 'FactoryMethodMatcher') -> None:
         self._factory_method_matcher = factory_method_matcher
@@ -77,10 +59,6 @@ class AbstractFactoryMatcher(Matcher):
 class AdapterMatcher(Matcher):
     """Adapter matcher."""
 
-    @property
-    def pattern_type(self) -> Type[Pattern]:
-        return Adapter
-
     def match(self, dg: Diagram, cls: Class) -> Iterable[Adapter]:
         if not cls.is_interface:
             return
@@ -101,10 +79,6 @@ class AdapterMatcher(Matcher):
 
 class BridgeMatcher(Matcher):
     """Bridge matcher."""
-
-    @property
-    def pattern_type(self) -> Type[Pattern]:
-        return Bridge
 
     def match(self, dg: Diagram, cls: Class) -> Iterable[Bridge]:
         if cls.is_interface:
@@ -135,10 +109,6 @@ class BridgeMatcher(Matcher):
 class CompositeMatcher(Matcher):
     """Composite matcher."""
 
-    @property
-    def pattern_type(self) -> Type[Pattern]:
-        return Composite
-
     def match(self, dg: Diagram, cls: Class) -> Iterable[Composite]:
         # Find leaves
         leaves = list(dg.get_realizations(cls) if cls.is_interface else dg.get_sub_classes(cls))
@@ -162,10 +132,6 @@ class CompositeMatcher(Matcher):
 
 class DecoratorMatcher(Matcher):
     """Decorator matcher."""
-
-    @property
-    def pattern_type(self) -> Type[Pattern]:
-        return Decorator
 
     def match(self, dg: Diagram, cls: Class) -> Iterable[Decorator]:
         # Find concrete components
@@ -196,10 +162,6 @@ class DecoratorMatcher(Matcher):
 class FacadeMatcher(Matcher):
     """Facade matcher."""
 
-    @property
-    def pattern_type(self) -> Type[Pattern]:
-        return Facade
-
     def match(self, dg: Diagram, cls: Class) -> Iterable[Facade]:
         # Find dependencies
         deps = list(dg.get_dependencies(cls))
@@ -211,10 +173,6 @@ class FactoryMethodMatcher(Matcher):
     """Factory method matcher."""
 
     _regex = re.compile(r'^(?:alloc|build|construct|create|instantiate|new)', re.I)
-
-    @property
-    def pattern_type(self) -> Type[Pattern]:
-        return FactoryMethod
 
     def match(self, dg: Diagram, cls: Class) -> Iterable[FactoryMethod]:
         # Find factory methods
@@ -237,10 +195,6 @@ class FactoryMethodMatcher(Matcher):
 
 class ProxyMatcher(Matcher):
     """Proxy matcher."""
-
-    @property
-    def pattern_type(self) -> Type[Pattern]:
-        return Proxy
 
     def match(self, dg: Diagram, cls: Class) -> Iterable[Proxy]:
         # Find proxies
